9f2b16110231acc268ab8ad02ce6545f7ec08ffa
Uziel Trujillo Colón <uziel.trujillo.mx@cloudappi.net>
2022-05-17 15:26:25 -0500

    Clase 01 	new file:   01_basicos.py 	new file:   02_condicionales.py

---
+++ 01_basicos.py
@@ -0,0 +1,26 @@
+# Imprimir en consola
+# print("Hola mundo")
+
+# Definicion de variables y constantes
+saludo = "Hola mundo"
+numero1 = 10
+numero2 = 20
+PI = 3.1416
+
+# Concatenar elementos
+# print( saludo +" "+ str(numero1) )
+# print( "%s %s" %(saludo, numero1) )
+
+# print( saludo, numero1, numero2, PI )
+
+# Entrada de datos
+# entrada = input("Escribe tu algo: ")
+# print( type( PI ), PI )
+
+profesor = "Uziel Trujillo Colon"
+menu = """
+  Bienvenidos al curso de induccion
+  impartdo por %s
+"""
+
+print( menu %(profesor) )
---
+++ 02_condicionales.py
@@ -0,0 +1,14 @@
+import random
+
+numeroAleatorio = random.randint( 0, 30 )
+# edad = int( input("Escribe tu edad: ") )
+
+# if( edad > 18 ):
+#   print("Eres mayor de edad")
+# elif( edad == 18 ):
+#   print("Eres mayor de edad recientemente")
+# else:
+#   print("Hueles a MP 😁")
+
+result = "Mayor de edad" if numeroAleatorio > 18 else "Menor de edad"
+print( result, numeroAleatorio )
